706029d30a1f4d73e1e64349a52e8c0dd09dadbe
YusufDarmawan <yusufdarmawan@students.amikom.ac.id>
2021-10-28 14:25:31 +0700

    Tugas Nunit testing

---
+++ TugasLibraryUnitTest/TugasTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class TugasTest
+    {
+        private Day day;
+        private User user;
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+        }
+        [Test]
+        public void Daytest()
+        {
+            Assert.AreEqual("Senin", day.NameOfDay(1));
+        }
+        [Test]
+        public void Usertest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+        }
+
+    }
+}
